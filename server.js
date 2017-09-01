cbc6381409c90303c47b09e0c710dec3b65948e0
Science001 <30399148+Science001@users.noreply.github.com>
2017-09-01 21:40:50 +0530

    Update: Added Search API

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/up.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'up.png'));
 });
 
+app.get('/searchicon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'searchicon.png'));
+});
+
 app.get('/favicon.png', function(req,res) {
 	res.sendFile(path.join(__dirname,'favicon.png'));
 });
@@ -34,7 +38,32 @@ app.get('/fetch-details', function(req, res) {
 
 	var id=parseInt(req.query.id);
 	res.send(JSON.stringify(pt.numbers[id]));
+});
 
+app.get('/search', function(req,res) {
+	var key=req.query.key;
+	var i=1;
+	if(isNaN(key)) {
+		if(key.length<=2) {
+			while(i<119) {
+				if(pt.numbers[i].symbol.toLowerCase() === key.toLowerCase()) res.send(JSON.stringify(pt.numbers[i]));
+				i++;
+			}
+			res.sendStatus(404);
+		}
+		else{
+			while(i<119){
+				if(pt.numbers[i].name.toLowerCase() === key.toLowerCase()) res.send(JSON.stringify(pt.numbers[i]));
+				i++;
+			}
+			res.sendStatus(404);
+		}
+	}
+	else {
+		var num=parseInt(key);
+		if(num>0 && num<119) res.send(pt.numbers[num]);
+		else res.sendStatus(404);
+	}
 });
 
 var port = 80;
